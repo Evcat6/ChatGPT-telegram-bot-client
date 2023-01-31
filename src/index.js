c3bfc79765e0d2826dc219bbd7f02fe5bf396fdf
unknown <demomusicevcat@gmail.com>
2023-01-31 23:44:44 +0100

    added greeting message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const { bot, webhookCallback } = require('./services/bot.service')
+const { authorTgLink } = require('./data/constants/chat')
+
+const greeting = async (ctx) => {
+  ctx.reply(`Hello! I'm ChatGPT telegram bot client created by <a href='${authorTgLink}' >Evgen Kotlyarchuck</a>!`, { parse_mode: 'HTML' })
+}
+
+// bot.stopPolling()
+
+bot.command('start', greeting)
 
 // Start the server
 if (process.env.NODE_ENV === 'production') {
