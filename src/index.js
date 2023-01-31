fbaa2130170e47bc8a74b88d64ea9db54069823a
unknown <demomusicevcat@gmail.com>
2023-01-31 22:39:23 +0100

    project linted

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const { Bot, webhookCallback } = require("grammy");
-require('dotenv').config();
+const express = require('express')
+const { Bot, webhookCallback } = require('grammy')
+require('dotenv').config()
 
-const bot = new Bot(process.env.BOT_CLIENT_API_KEY);
-
+const bot = new Bot(process.env.BOT_CLIENT_API_KEY)
 
 // Start the server
-if (process.env.NODE_ENV === "production") {
-    // Use Webhooks for the production server
-    const app = express();
-    app.use(express.json());
-    app.use(webhookCallback(bot, "express"));
-  
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Bot listening on port ${PORT}`);
-    });
+if (process.env.NODE_ENV === 'production') {
+  // Use Webhooks for the production server
+  const app = express()
+  app.use(express.json())
+  app.use(webhookCallback(bot, 'express'))
+
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`)
+  })
 } else {
-    // Use Long Polling for development
-    bot.start();
+  // Use Long Polling for development
+  bot.start()
 }
