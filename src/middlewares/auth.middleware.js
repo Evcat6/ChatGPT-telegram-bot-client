c2162a5d67e572033ecf51f0e91f54012d5a1cb9
unknown <demomusicevcat@gmail.com>
2023-02-01 12:59:56 +0100

    edited auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,20 +1,21 @@
+const { updateStorage } = require('../services/session.service')
 require('dotenv').config()
 
 async function authMiddleware (ctx, next) {
-  if (ctx.message.text === process.enc.USER_PASSWORD) {
-    await next()
-  }
-  ctx.reply('Invalid password')
-}
-
-async function checkAuthMiddleware (ctx, next) {
   if (ctx.session.authorized) {
     return await next()
   }
-  ctx.reply('You are not authorized Please enter a password:')
+  if (ctx.session.authorized && ctx.message.text === process.env.USER_PASSWORD) {
+    ctx.reply('You have already authorized')
+    return await next()
+  }
+  if (ctx.message.text === process.env.USER_PASSWORD) {
+    updateStorage(ctx, 'authorized', true)
+    return ctx.reply('Congratulations, you have successfully logged in and now you can use CatGPT')
+  }
+  ctx.reply('Invalid password or you are not authorized')
 }
 
 module.exports = {
-  authMiddleware,
-  checkAuthMiddleware
+  authMiddleware
 }
