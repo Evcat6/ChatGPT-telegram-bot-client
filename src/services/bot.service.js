d1697617c6e56450e56482e27e2109f71562d8ea
unknown <demomusicevcat@gmail.com>
2023-02-01 11:03:42 +0100

    added session storage to bot.service.js

--- src/services/bot.service.js
+++ src/services/bot.service.js
@@ -1,7 +1,14 @@
-const { Bot, webhookCallback } = require('grammy')
+const { Bot, webhookCallback, session } = require('grammy')
+const { createInitialSessionData } = require('./session.service')
+const { freeStorage } = require('@grammyjs/storage-free')
 require('dotenv').config()
 
 const bot = new Bot(process.env.BOT_CLIENT_API_KEY)
+
+bot.use(session({
+  initial: createInitialSessionData,
+  storage: freeStorage(bot.token)
+}))
 
 module.exports = {
   bot,
