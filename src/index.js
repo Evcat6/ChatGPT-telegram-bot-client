f15cde65d05aa5e2759373391110d593eb83ef1c
unknown <demomusicevcat@gmail.com>
2023-02-01 13:14:04 +0100

    edited index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 const express = require('express')
 const { bot, webhookCallback } = require('./services/bot.service')
-const { authorTgLink } = require('./data/constants/chat')
 const { authMiddleware } = require('./middlewares/auth.middleware')
-const { clearStorage } = require('./services/session.service')
-const { createRequest } = require('./services/request.service')
-
-const greeting = async (ctx) => {
-  ctx.reply(`Hello! I'm ChatGPT telegram bot client created by <a href='${authorTgLink}' >Evgen Kotlyarchuck</a>!`, { parse_mode: 'HTML' })
-}
-
-const stop = async (ctx) => {
-  clearStorage(ctx)
-  ctx.reply(`Goodbye ${ctx.message.chat.username})`)
-}
-
-const serverRequest = async (ctx) => {
-  const resultQuery = await createRequest(ctx.message.text)
-  ctx.reply(resultQuery)
-}
+const { greeting, stop, serverRequest } = require('./controllers/bot.controller')
 
 bot.command('start', greeting)
 
