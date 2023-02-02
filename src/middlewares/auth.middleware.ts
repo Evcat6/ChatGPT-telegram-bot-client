94ffc50f5a9b81fc81ba85b395a7cdab73b2c1b2
unknown <demomusicevcat@gmail.com>
2023-02-02 22:14:08 +0100

    added ts middlewares

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv'
+import { type NextFunction } from 'grammy'
+import { updateStorage } from '../services/session.service'
+import { type MyContext } from '../types/context'
+// 'CommandMiddleware<Context>'
+
+dotenv.config()
+
+async function authMiddleware (ctx: MyContext, next: NextFunction): Promise<any> {
+  if (ctx.session.authorized) {
+    void next()
+  }
+  if (ctx.session.authorized && ctx.message.text === process.env.USER_PASSWORD) {
+    void ctx.reply('You have already authorized')
+    void next()
+  }
+  if (ctx.message.text === process.env.USER_PASSWORD) {
+    updateStorage(ctx, 'authorized', true)
+    void ctx.reply('Congratulations, you have successfully logged in and now you can use CatGPT')
+  }
+  void ctx.reply('Invalid password or you are not authorized')
+}
+
+export { authMiddleware }
