70441355ec0c479a6a1a5962aaf67f88613ea51f
unknown <demomusicevcat@gmail.com>
2023-02-03 09:41:28 +0100

    fixed auth middleware problems

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -8,17 +8,20 @@ dotenv.config()
 
 async function authMiddleware (ctx: MyContext, next: NextFunction): Promise<any> {
   if (ctx.session.authorized) {
-    void next()
+    void next(); return
   }
   if (ctx.session.authorized && ctx.message.text === process.env.USER_PASSWORD) {
     void ctx.reply('You have already authorized')
-    void next()
+    void next(); return
   }
   if (ctx.message.text === process.env.USER_PASSWORD) {
     updateStorage(ctx, 'authorized', true)
     void ctx.reply('Congratulations, you have successfully logged in and now you can use CatGPT')
+    return
   }
-  void ctx.reply('Invalid password or you are not authorized')
+  if (!ctx.session.authorized) {
+    void ctx.reply('Invalid password or you are not authorized')
+  }
 }
 
 export { authMiddleware }
